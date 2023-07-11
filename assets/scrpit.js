9d4776e28871739d216e23d56a4cd37750443c9a
Cristofferb7 <136852733+Cristofferb7@users.noreply.github.com>
2023-07-11 14:46:45 -0400

    hw startup on js

--- assets/scrpit.js
+++ assets/scrpit.js
@@ -13,4 +13,14 @@
 
 // function to display current weather data from fetch request
 
-// 
+// function to display the 5 day forecast data from fetch request 
+// need icon, temp, humidity, date , wind speed
+
+// create element for each of the above^^
+
+// function to display 5 day forecast data from fetch request for each card
+
+// function to specifically target geolocation with lat and lon and use function above
+// to make calls here maybe a call back function 
+
+// function to event listener for search button 
