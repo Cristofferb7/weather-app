e1ff826593dfd033d3d9e9dc867d10589774b02a
Cristofferb7 <136852733+Cristofferb7@users.noreply.github.com>
2023-07-11 14:42:57 -0400

    startup

---
+++ assets/scrpit.js
@@ -0,0 +1,16 @@
+// 3 global variables 
+// 1 for the API KEY
+// 1 for base url : https://openweathermap.org/
+// search history array 
+
+// function to display the search history 
+// loop over history array and count down so the most recent search is at the top
+
+// function to update search history on local storage
+
+
+// function to get search history from local storage
+
+// function to display current weather data from fetch request
+
+// 
