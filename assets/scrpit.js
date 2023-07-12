7ea5e6f573f483bb98a8a9a6de2b617258e27046
Cristofferb7 <136852733+Cristofferb7@users.noreply.github.com>
2023-07-12 18:00:44 -0400

    added containers and made a for loop for the days to go by 5, and also created elements on the script to show when i click on the search button

--- assets/scrpit.js
+++ assets/scrpit.js
@@ -33,7 +33,7 @@ function search() {
     var content = searchCity.value
     console.log(content)
 
-    var url = "https://api.openweathermap.org/data/2.5/forecast?q="+ content +"&appid=" + APIKey
+    var url = "https://api.openweathermap.org/data/2.5/forecast?q="+ content +"&appid=" + APIKey + "&units=imperial"
 
 
     console.log(url)
@@ -45,18 +45,76 @@ function search() {
     .then(function(data) {
         console.log(data);
 
+        // current weather
+
+        var currentDay =  data.list[0];
+
+        console.log(currentDay)
+
+        var currentDateH2 = document.createElement("h2");
+        var currentTempP = document.createElement("p");
+
+        currentDateH2.textContent = data.city.name
+        currentTempP.textContent = "Temp: " + currentDay.main.temp
+
+        var currentDayContainer = document.getElementById("current-weather");
+        currentDayContainer.append(currentDateH2, currentTempP)
+
+
+
+        // five day forecast
+
         var fiveDays = [
-            data.list[0],
-            data.list[8],
-            data.list[16],
-            data.list[24],
-            data.list[32],
+            data.list[1],
+            data.list[9],
+            data.list[17],
+            data.list[25],
+            data.list[33],
         ]
 
         console.log(fiveDays)
 
+        // var temp1 = document.getElementById("temp1");
+        // temp1.textContent = fiveDays[0].main.temp
+
+        //  var temp2 = document.getElementById("temp2");
+        // temp2.textContent = fiveDays[1].main.temp
+
+            // <div class="card" style="width: 18rem;">
+            //  <div class="card-header">
+            //      Featured
+            //  </div>
+            //  <ul class="list-group list-group-flush">
+            //     <li class="c">Temp/li>
+            //     <li class="list-group-item">A second item</li>
+            //     <li class="list-group-item">A third item</li>
+            //   </ul>
+            // </div>
+
         for(i = 0; i < fiveDays.length; i++) {
+            var containerDiv = document.createElement("div");
+            var dateH3 = document.createElement("h3");
+
+            var newUl = document.createElement("ul");
+            var tempLi = document.createElement("li");
+
+            containerDiv.classList.add("card");
+            containerDiv.classList.add("col");
+            dateH3.textContent = fiveDays[i].dt_txt
+
+            newUl.classList.add("list-group")
+            newUl.classList.add("list-group-flush")
+            tempLi.textContent = "Temp: " + fiveDays[i].main.temp
+            tempLi.classList.add("list-group-item")
+
+            newUl.appendChild(tempLi)
             
+            containerDiv.appendChild(dateH3)
+            containerDiv.appendChild(newUl)
+            
+
+            var fiveDayContainer = document.getElementById("five-days");
+            fiveDayContainer.appendChild(containerDiv)
         }
 
 
